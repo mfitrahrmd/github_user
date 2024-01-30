41f2747a2a1b71004dbc568366b0a5edef854295
mfitrahrmd <tgcfitrah26@gmail.com>
2024-01-30 12:16:33 +0700

    implement user remote repository

---
+++ app/src/main/java/com/mfitrahrmd/githubuser/repositories/remote/UserRemoteRepository.kt
@@ -0,0 +1,41 @@
+package com.mfitrahrmd.githubuser.repositories.remote
+
+import com.mfitrahrmd.githubuser.models.User
+import com.mfitrahrmd.githubuser.repositories.UserRepository
+import com.mfitrahrmd.githubuser.repositories.remote.responsemodels.toUser
+import com.mfitrahrmd.githubuser.repositories.remote.services.GithubService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class UserRemoteRepository : UserRepository() {
+    private val _githubService = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(GithubService::class.java)
+
+    companion object {
+        const val BASE_URL = "https://api.github.com/"
+    }
+
+    override suspend fun searchUsers(query: String): List<User>? {
+        val res = _githubService.searchUsers(query)
+
+        return res.body()?.toUser()
+    }
+
+    override suspend fun findUserByUsername(username: String): User? {
+        val res = _githubService.findUserByUsername(username)
+
+        return res.body()?.toUser()
+    }
+
+    override suspend fun listUserFollowers(username: String): List<User>? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun listUserFollowing(username: String): List<User>? {
+        TODO("Not yet implemented")
+    }
+
+}
