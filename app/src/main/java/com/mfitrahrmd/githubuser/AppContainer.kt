5695ec35ddec5d48b08b6ce48bc53439380d9f95
mfitrahrmd <tgcfitrah26@gmail.com>
2024-01-30 12:18:44 +0700

    implement app container

---
+++ app/src/main/java/com/mfitrahrmd/githubuser/AppContainer.kt
@@ -0,0 +1,15 @@
+package com.mfitrahrmd.githubuser
+
+import android.content.Context
+import com.mfitrahrmd.githubuser.repositories.UserRepository
+import com.mfitrahrmd.githubuser.repositories.remote.UserRemoteRepository
+
+interface AppContainer {
+    val userRepository: UserRepository
+}
+
+class AppDataContainer(private val _context: Context): AppContainer {
+    override val userRepository: UserRepository by lazy {
+        UserRemoteRepository()
+    }
+}
