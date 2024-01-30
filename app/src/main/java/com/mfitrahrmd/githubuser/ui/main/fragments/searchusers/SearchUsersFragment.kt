793e1930da921af0dfa5cd3a4f3a97a6cd991f26
mfitrahrmd <tgcfitrah26@gmail.com>
2024-01-30 22:20:10 +0700

    add spacing decorator to popular-indo-users adapter

--- app/src/main/java/com/mfitrahrmd/githubuser/ui/main/fragments/searchusers/SearchUsersFragment.kt
+++ app/src/main/java/com/mfitrahrmd/githubuser/ui/main/fragments/searchusers/SearchUsersFragment.kt
@@ -1,5 +1,6 @@
 package com.mfitrahrmd.githubuser.ui.main.fragments.searchusers
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +12,9 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.TransitionInflater
+import com.mfitrahrmd.githubuser.R
 import com.mfitrahrmd.githubuser.databinding.FragmentSearchUsersBinding
 import com.mfitrahrmd.githubuser.ui.AppViewModelProvider
 import com.mfitrahrmd.githubuser.ui.UiState
@@ -21,12 +24,14 @@ class SearchUsersFragment : Fragment() {
     private lateinit var _binding: FragmentSearchUsersBinding
     private val _viewModel: SearchUsersViewModel by viewModels(factoryProducer = { AppViewModelProvider.Factory })
     private val _searchUsersAdapter: SearchUsersAdapter = SearchUsersAdapter(emptyList())
-    private val _popularIndoUsersAdapter: PopularIndoUsersAdapter = PopularIndoUsersAdapter(emptyList())
+    private val _popularIndoUsersAdapter: PopularIndoUsersAdapter =
+        PopularIndoUsersAdapter(emptyList())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        sharedElementEnterTransition = TransitionInflater.from(requireContext()).inflateTransition(android.R.transition.move)
+        sharedElementEnterTransition =
+            TransitionInflater.from(requireContext()).inflateTransition(android.R.transition.move)
     }
 
     override fun onCreateView(
@@ -45,7 +50,10 @@ class SearchUsersFragment : Fragment() {
         observe(_searchUsersAdapter, _popularIndoUsersAdapter)
     }
 
-    private fun bind(searchUsersAdapter: SearchUsersAdapter, popularIndoUsersAdapter: PopularIndoUsersAdapter) {
+    private fun bind(
+        searchUsersAdapter: SearchUsersAdapter,
+        popularIndoUsersAdapter: PopularIndoUsersAdapter
+    ) {
         with(_binding) {
             /*
             * Setup search view
@@ -61,19 +69,59 @@ class SearchUsersFragment : Fragment() {
                     }
                     false
                 }
-            rvSearchUsers.layoutManager = LinearLayoutManager(this@SearchUsersFragment.context, LinearLayoutManager.VERTICAL, false)
+            rvSearchUsers.layoutManager = LinearLayoutManager(
+                this@SearchUsersFragment.context,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
             rvSearchUsers.adapter = searchUsersAdapter
 
-            rvPopularIndoUsers.layoutManager = LinearLayoutManager(this@SearchUsersFragment.context, LinearLayoutManager.HORIZONTAL, false)
+            rvPopularIndoUsers.layoutManager = LinearLayoutManager(
+                this@SearchUsersFragment.context,
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
             rvPopularIndoUsers.adapter = popularIndoUsersAdapter
+            rvPopularIndoUsers.addItemDecoration(object : RecyclerView.ItemDecoration() {
+                override fun getItemOffsets(
+                    outRect: Rect,
+                    view: View,
+                    parent: RecyclerView,
+                    state: RecyclerView.State
+                ) {
+                    super.getItemOffsets(outRect, view, parent, state)
+
+                    val position = parent.getChildLayoutPosition(view)
+
+                    when(position) {
+                        0 -> {
+                            outRect.right =
+                                resources.getDimensionPixelSize(R.dimen.popular_indo_users_spacing)
+                        }
+                        parent.childCount - 1 -> {
+                            outRect.left =
+                                resources.getDimensionPixelSize(R.dimen.popular_indo_users_spacing)
+                        }
+                        else -> {
+                            outRect.right =
+                                resources.getDimensionPixelSize(R.dimen.popular_indo_users_spacing)
+                            outRect.left =
+                                resources.getDimensionPixelSize(R.dimen.popular_indo_users_spacing)
+                        }
+                    }
+                }
+            })
         }
     }
 
-    private fun observe(searchUsersAdapter: SearchUsersAdapter, popularIndoUsersAdapter: PopularIndoUsersAdapter) {
+    private fun observe(
+        searchUsersAdapter: SearchUsersAdapter,
+        popularIndoUsersAdapter: PopularIndoUsersAdapter
+    ) {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 _viewModel.searchUsersState.collect { currentUiState ->
-                    when(currentUiState) {
+                    when (currentUiState) {
                         is UiState.Success -> {
                             searchUsersAdapter.setUsers {
                                 currentUiState.data ?: emptyList()
@@ -84,6 +132,7 @@ class SearchUsersFragment : Fragment() {
                                 rvSearchUsers.visibility = View.VISIBLE
                             }
                         }
+
                         is UiState.Loading -> {
                             with(_binding) {
                                 shimmerSearchUsers.startShimmer()
@@ -91,12 +140,17 @@ class SearchUsersFragment : Fragment() {
                                 rvSearchUsers.visibility = View.GONE
                             }
                         }
+
                         is UiState.Error -> {
                             with(_binding) {
                                 shimmerSearchUsers.stopShimmer()
                                 shimmerSearchUsers.visibility = View.GONE
                                 rvSearchUsers.visibility = View.GONE
-                                Toast.makeText(view?.context, if (!currentUiState.message.isNullOrEmpty()) currentUiState.message else DEFAULT_ERROR_MESSAGE, Toast.LENGTH_SHORT).show()
+                                Toast.makeText(
+                                    view?.context,
+                                    if (!currentUiState.message.isNullOrEmpty()) currentUiState.message else DEFAULT_ERROR_MESSAGE,
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
                         }
                     }
@@ -106,7 +160,7 @@ class SearchUsersFragment : Fragment() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 _viewModel.popularIndoUsersState.collect { currentUiState ->
-                    when(currentUiState) {
+                    when (currentUiState) {
                         is UiState.Success -> {
                             popularIndoUsersAdapter.setPopularIndoUsers {
                                 currentUiState.data ?: emptyList()
@@ -117,6 +171,7 @@ class SearchUsersFragment : Fragment() {
                                 rvPopularIndoUsers.visibility = View.VISIBLE
                             }
                         }
+
                         is UiState.Loading -> {
                             with(_binding) {
                                 shimmerPopularIndoUsers.startShimmer()
@@ -124,12 +179,17 @@ class SearchUsersFragment : Fragment() {
                                 rvPopularIndoUsers.visibility = View.GONE
                             }
                         }
+
                         is UiState.Error -> {
                             with(_binding) {
                                 shimmerPopularIndoUsers.stopShimmer()
                                 shimmerPopularIndoUsers.visibility = View.GONE
                                 rvPopularIndoUsers.visibility = View.GONE
-                                Toast.makeText(view?.context, if (!currentUiState.message.isNullOrEmpty()) currentUiState.message else DEFAULT_ERROR_MESSAGE, Toast.LENGTH_SHORT).show()
+                                Toast.makeText(
+                                    view?.context,
+                                    if (!currentUiState.message.isNullOrEmpty()) currentUiState.message else DEFAULT_ERROR_MESSAGE,
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
                         }
                     }
